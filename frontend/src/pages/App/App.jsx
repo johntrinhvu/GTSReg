22e79223cde2e499c06e4aa1eee5d09de71139b3
johntrinhvu <johntrinhvu@yahoo.com>
2025-04-29 19:48:28 -0700

    add tailwind to the src

---
+++ frontend/src/pages/App/App.jsx
@@ -0,0 +1,9 @@
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="pt-24 text-center">
+      <h1 className="text-4xl font-bold">Hello GTS</h1>
+    </div>
+  );
+}
