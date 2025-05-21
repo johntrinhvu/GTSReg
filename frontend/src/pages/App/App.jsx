8f7d0f9c53e5a06e813fc50afe3710a68c0b619e
johntrinhvu <johntrinhvu@yahoo.com>
2025-05-21 14:22:21 -0700

    make header

--- frontend/src/pages/App/App.jsx
+++ frontend/src/pages/App/App.jsx
@@ -1,9 +1,11 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "../../components/Header/Header.jsx";
 import './App.css';
 
 export default function App() {
   return (
-    <div className="pt-24 text-center">
-      <h1 className="text-4xl font-bold">Hello GTS</h1>
+    <div className="text-center">
+      <Header />
     </div>
   );
 }
